6ddaf42519061acad5f577053bf9d2e1ed5bb896
kev-n14 <kevinfinn688@gmail.com>
2023-04-13 13:46:33 +0000

    add functionality to add items to database via index page

--- expenseapp/views.py
+++ expenseapp/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .forms import ExpenseForm
-
+from .models import Expenses
 
 def index(request):
     if request.method == "POST":
         expense = ExpenseForm(request.POST)
         if expense.is_valid():
             expense.save()
-
+    expenses = Expenses.objects.all()
     expense_form = ExpenseForm()
-    return render(request, 'expenseapp/index.html', {'expense_form':expense_form})
+    return render(request, 'expenseapp/index.html', {'expense_form':expense_form, 'expenses':expenses})
