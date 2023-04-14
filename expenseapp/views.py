bb63927fbc0a5ccaf6c9245ae6cd8976b01431d0
kev-n14 <kevinfinn688@gmail.com>
2023-04-14 18:25:59 +0000

    authenticate user

--- expenseapp/views.py
+++ expenseapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from .forms import ExpenseForm, CreateUserForm
 from .models import Expenses
 from django.db.models import Sum
@@ -9,41 +10,53 @@ import datetime
 
 
 def registerPage(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = CreateUserForm()
 
-    if request.method == "POST":
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            user = form.cleaned_data.get('username')
-            messages.success(request, 'Account was created for '+ user)
+        if request.method == "POST":
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for '+ user)
 
-            return redirect('login')
+                return redirect('login')
 
     context = {'form': form}
     return render(request, 'expenseapp/register.html', context)
 
 
 def loginPage(request):
-    if request.method == "POST":
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == "POST":
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
-        print(user)
-        if user is not None:
-            login(request, user)
-            
-            return redirect('index')
+            user = authenticate(request, username=username, password=password)
+            print(user)
+            if user is not None:
+                login(request, user)
+                
+                return redirect('index')
 
-        else:
-            messages.info(request, 'Username or Password is incorrect')
+            else:
+                messages.info(request, 'Username or Password is incorrect')
 
     context = {}
     
     return render(request, 'expenseapp/login.html', context)
 
 
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required(login_url='login')
 def index(request):
     if request.method == "POST":
         expense = ExpenseForm(request.POST)
@@ -77,7 +90,7 @@ def index(request):
         'yearly_sum': yearly_sum, 'monthly_sum': monthly_sum, 'weekly_sum': weekly_sum,
         'daily_sums': daily_sums, 'categorical_sums': categorical_sums})
 
-
+@login_required(login_url='login')
 def edit(request, id):
     expense = Expenses.objects.get(id=id)
     expense_form = ExpenseForm(instance=expense)
